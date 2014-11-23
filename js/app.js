d294e9ccf52732d625964801ee2cf995cb300e43
Shaw Ken Chang <digitalkc408@gmail.com>
2014-11-22 19:05:45 -0800

    Add more variables for ease of debugging
    fixed some issues with getting the total percentage.
    add few variables such as totalPoints, maxPoints, and currentPercentage
    to debug the code
    made default total max points to 100 for ease of debugging

--- js/app.js
+++ js/app.js
@@ -2,30 +2,70 @@
  "use strict";
  
  var gApoint = 90.0;
+ var gBpoint = 80.0;
+ var gCpoint = 70.0;
+ var gDpoint = 60.0;
  
  var computeGrade = function()
  {
-    var currentPoints = Number( $('#Homeworks').val() + $('#Projects').val() + $('#Labs').val() + $('#Presentation').val() + $('#Midterm').val() + $('#Final').val());
-    var currentGrade = "NA";
+   //variables for getting points for each category
+   var homeworks = Number($('#Homeworks').val());
+   var labs = Number($('#Labs').val());
+   var project = Number($('#Project').val());
+   var presentation = Number($('#Presentation').val());
+   var midterm = Number($('#Midterm').val());
+   var final = Number($('#Final').val());
+   
+   //variables for getting max points for each category
+   var homeworksMaxPoints = Number($('#HomeworksMax').val());
+   var labsMaxPoints = Number($('#LabsMax').val());
+   var projectMaxPoints = Number($('#ProjectMax').val());
+   var presentationMaxPoints = Number($('#PresentationMax').val());
+   var midtermMaxPoints = Number($('#MidtermMax').val());
+   var finalMaxPoints = Number($('#FinalMax').val());
+   
+   //variables to store the sum of points and calculate the current percentage
+   var totalPoints = homeworks+labs+project+presentation+midterm+final;
+   var maxPoints = homeworksMaxPoints+labsMaxPoints+projectMaxPoints+presentationMaxPoints+midtermMaxPoints+finalMaxPoints;
+   var currentPercentage = (totalPoints/maxPoints)*100;
+   
+   
+   var currentGrade = "NA";
  
-    if (currentPoints >= gApoint)
+    if (currentPercentage >= gApoint)
     {
         currentGrade = "A";
-    }
-    else
-    {
+    }else if (currentPercentage >= gBpoint){
+        currentGrade = "B";
+    }else if (currentPercentage >= gCpoint){
+         currentGrade = "C";
+    }else if (currentPercentage >= gDpoint){
+         currentGrade = "D";
+    }else{
         currentGrade = "F";
     }
-    $('#finalgrade').text(currentGrade);
+    $('#finalGrade').text(currentGrade);
+    $('#totalPoints').text(totalPoints.toString());
+    $('#maxPoints').text(maxPoints.toString());
+    $('#finalPercentage').text(currentPercentage.toString());
  };
  
  var saveSettings = function()
  {
     try {
-        var aPoint = parseFloat( $('#gradeCutOff').val() );
+        var aPoint = Number( $('#gradeA').val() );
+        var bPoint = Number( $('#gradeB').val() );
+        var cPoint = Number( $('#gradeC').val() );
+        var dPoint = Number( $('#gradeD').val() );
  
-        localStorage.setItem('gradeCutOff', aPoint);
+        localStorage.setItem('gradeA', aPoint);
+        localStorage.setItem('gradeB', bPoint);
+        localStorage.setItem('gradec', cPoint);
+        localStorage.setItem('gradeD', dPoint);
         gApoint = aPoint;
+        gBpoint = bPoint;
+        gCpoint = cPoint;
+        gDpoint = dPoint;
         window.history.back();
     } catch (ex)
     {
@@ -46,14 +86,32 @@
                   $('#saveSettings').on('click', saveSettings);
                   $('#cancelSettings').on('click', cancelSettings);
 
-                  var gradeCutOffSetting = localStorage.getItem('gradeCutOff');
+                  var gradeCutOffSettingA = localStorage.getItem('gradeA');
+                  var gradeCutOffSettingB = localStorage.getItem('gradeB');
+                  var gradeCutOffSettingC = localStorage.getItem('gradeB');
+                  var gradeCutOffSettingD = localStorage.getItem('gradeD');
                   
-                  if (gradeCutOffSetting)
+                  if (gradeCutOffSettingA)
                   {
-                    gApoint = parseFloat(gradeCutOffSetting);
+                     gApoint = Number(gradeCutOffSettingA);
+                  }
+                  if (gradeCutOffSettingB)
+                  {
+                     gBpoint = Number(gradeCutOffSettingB);
+                  }
+                  if (gradeCutOffSettingC)
+                  {
+                     gCpoint = Number(gradeCutOffSettingC);
+                  }
+                  if (gradeCutOffSettingD)
+                  {
+                     gDpoint = Number(gradeCutOffSettingD);
                   }
                   
-                  $('#gradeCutOff').val(gApoint);
+                     Number($('#gradeA').val(gApoint));
+                     Number($('#gradeB').val(gBpoint));
+                     Number($('#gradeC').val(gCpoint));
+                     Number($('#gradeD').val(gDpoint));
                   
                   });
 
